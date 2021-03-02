83e3203e9d0a34faf7c67ecaaa764e04d7663e01
«Nikita <mrnikita.harkov@gmail.com>
2021-03-02 19:41:03 +0200

    Stripe checkout Initial imports

--- src/components/cart/StripeCheckout.js
+++ src/components/cart/StripeCheckout.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  CardElement,
+  useStripe,
+  Elements,
+  useElements,
+} from '@stripe/react-stripe-js';
+import axios from 'axios';
+import { useCartContext } from '../../context/cart_context';
+import { useUserContext } from '../../context/user_context';
+import { formatPrice } from '../../utils/helpers';
+import { useHistory } from 'react-router-dom';
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
   return <h2>hello from stripe checkout</h2>;
@@ -7,9 +21,11 @@ const CheckoutForm = () => {
 
 const StripeCheckout = () => {
   return (
-    <div>
-      <CheckoutForm />
-    </div>
+    <Wrapper>
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+    </Wrapper>
   );
 };
 
